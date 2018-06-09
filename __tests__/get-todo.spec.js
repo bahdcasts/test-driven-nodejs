fb9a12359721a7716ef23263d5a4205f25cf2d50
Kati Frantz <frantzenterprises777@gmail.com>
2018-06-09 23:13:50 +0100

    test for exceptions on single todo route

--- __tests__/get-todo.spec.js
+++ __tests__/get-todo.spec.js
@@ -20,4 +20,14 @@ describe('get single todo', () => {
       id: expect.any(String)
     })
   })
+
+  test('receives error message if todo was not found', async () => {
+    // arrange
+    const id = 'FAKE_INVALID_ID'
+    // get invalid todo
+    const response = await server.get(`/todo/${id}`)
+    // assert the error message from server.
+    expect(response.status).toBe(404)
+    expect(response.body.message).toBe('Todo not found.')
+  })
 })
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,14 @@ const Todo = require('../database/models/Todo')
 
 module.exports = app => {
   app.get('/todo/:id', async (req, res) => {
-    const todo = await Todo.findById(req.params.id)
+    try {
+      const todo = await Todo.findById(req.params.id)
 
-    res.json(todo)
+      res.json(todo)
+    } catch (error) {
+      return res.status(404).json({ message: 'Todo not found.' })
+    }
+
   })
   app.post('/todos', async (req, res) => {
     const todo = await Todo.create(req.body)
